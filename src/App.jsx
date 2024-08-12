b01021ce5ac32ffe94c32a34a8ecc69728425b4c
Marta Hilgert <marta.hil.1988@gmail.com>
2024-08-12 18:54:27 -0300

    00:52:49 conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ function App() {
 
 export default App*/
 
+//import UserGreeting from "./UserGreeting";
+
 /* 2)
 import Card from './Card.jsx'
 
@@ -47,6 +49,7 @@ function App() {
 
 export default App*/
 
+/* 4) 
 import Student from './Student.jsx'
 
 function App() {
@@ -54,15 +57,31 @@ function App() {
     <>
       <Student name='Spongebob' age={30} isStudent={true}/>
 
-      {/*if age is nor a number ('30' is a string), the console will show a Warning:
-      <Student name='Spongebob' age='30' isStudent={true}/>
-      react-jsx-dev-runtime.development.js:87  Warning: Failed prop type: 
-      Invalid prop `age` of type `string` supplied to `Student`, expected `number`.*/}
+      {//if age is nor a number ('30' is a string), the console will show a Warning:
+      //<Student name='Spongebob' age='30' isStudent={true}/>
+      //react-jsx-dev-runtime.development.js:87  Warning: Failed prop type: 
+      //Invalid prop `age` of type `string` supplied to `Student`, expected `number`.}
 
       <Student name='Patrick' age={42} isStudent={false}/>
       <Student name='Squidward' age={50} isStudent={false}/>
       <Student name='Sandy' age={27} isStudent={true}/>
       <Student name='Larry'/>
+    </>
+  ); 
+}
+
+export default App*/
+
+import UserGreeting from './UserGreeting.jsx'
+
+function App() {
+  return(
+    <>
+      <UserGreeting isLoggedIn={true} username="BroCode"/>      
+
+      {//<UserGreeting isLoggedIn={false} username="BroCode"/>
+      //if isLoggedIn={false}, "Please log in to continue" will appear in the webpage
+      }    
     </>
   ); 
 }
